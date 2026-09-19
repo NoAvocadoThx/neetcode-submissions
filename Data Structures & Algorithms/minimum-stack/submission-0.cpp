93a9daa9cd10d4fe07eb5a9e688dfeb2cd7d5707
Zhihao(Michael) Wang <wildpepsi0808@gmail.com>
2026-09-19 10:54:22 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,11 @@
+class MinStack {
+    stack<pair<int, int>> st;   // {val, 当前最小值}
+public:
+    void push(int val) {
+        int mn = st.empty() ? val : min(val, st.top().second);
+        st.push({val, mn});
+    }
+    void pop()    { st.pop(); }
+    int top()     { return st.top().first; }
+    int getMin()  { return st.top().second; }
+};
